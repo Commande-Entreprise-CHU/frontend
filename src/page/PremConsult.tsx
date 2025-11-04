c922efea790b70b2cc442bed01697a954b07387f
Pautet AUrélien <aurelien.pautet@imt-atlantique.net>
2025-11-04 11:49:52 +0100

    Fix the json import for vercel build

--- src/page/PremConsult.tsx
+++ src/page/PremConsult.tsx
@@ -1,8 +1,9 @@
 import DynamicForm from "../components/DynamicForm";
-import PremConsultConfig from "../utils/PremConsult.json";
+import PremConsultConfig from "../utils/PremConsult.json" assert { type: "json" };
+import type { FormConfig } from "../types/formTypes";
 
 const PremConsult = () => {
-  return <DynamicForm config={PremConsultConfig} />;
+  return <DynamicForm config={PremConsultConfig as FormConfig} />;
 };
 
 export default PremConsult;
--- src/page/PremConsultDyn.tsx
+++ src/page/PremConsultDyn.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import PremConsult from "../utils/PremConsult.json";
+import PremConsult from "../utils/PremConsult.json" assert { type: "json" };
+import type { FormConfig } from "../types/formTypes";
 import DynamicForm from "../components/DynamicForm";
 
 const PremConsultDyn: React.FC = () => {
-  return <DynamicForm config={PremConsult} />;
+  return <DynamicForm config={PremConsult as FormConfig} />;
 };
 
 export default PremConsultDyn;
